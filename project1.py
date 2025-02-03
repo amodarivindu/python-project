e8773839ee934b94da454e185550e71d72a61eb7
Amoda Dharmasiri <amoda.dharmasiri@globalwavenet.com>
2025-02-03 15:44:22 +0530

    final one

--- project1.py
+++ project1.py
@@ -9,8 +9,8 @@ def roll():
 
     return roll
 
-value = roll()
-print(value)
+#value = roll()
+#print(value)
 
 while True:
     players = input("Enter number of players: ")
@@ -24,4 +24,36 @@ while True:
         print("Number of players must be a digit")
 
 print("This is the correct number of players:", players)
+
+max_score = 50
+player_scores = [0 for _ in range(players)]
+print(player_scores)
+
+
+while max(player_scores) < max_score:
     
+    for palyer_idx in range(players): # player_idx is the index of the player
+        print("Player", palyer_idx + 1, "'s turn")
+        current_score = 0
+
+        while True:
+            should_roll = input("Do you want to roll? (y)?: ")
+            if should_roll.lower() != "y":
+                break
+            value = roll()
+            if value == 1:
+                print("you rolled a 1! Turn done!")
+                current_score = 0
+                break
+            else:    
+                current_score += value
+                print("You rolled a", value)
+            
+            print("Your total score is:", current_score)
+
+        player_scores[palyer_idx] += current_score
+        print("Player", palyer_idx + 1, "scored", current_score, "points. Total:", player_scores[palyer_idx])
+
+max_score = max(player_scores)
+winning_idx = player_scores.index(max_score)
+print("Player", winning_idx + 1, "wins with a score of", max_score)
